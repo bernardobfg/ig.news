3be7425184310115a7fcb919678eb74b2a2d6ec0
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-09-25 17:57:15 -0300

    stype payments configured

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,39 @@
+import { signin, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
-interface SubscribeButtonProps{
+interface SubscribeButtonProps {
     priceId: string
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-        >
-            Subscribe now
-        </button>
-    )
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession()
+    async function handleSubscribe() {
+        if (!session) {
+            signin('github')
+            return;
+        }
+        try {
+            const response = await api.post("/subscribe");
+            const { sessionId } = response.data;
+            console.log(sessionId)
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId});
+        }
+        catch(err) {
+            alert(err.message);
+        }
+    }
+
+return (
+    <button
+        type="button"
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+    >
+        Subscribe now
+    </button>
+)
 }
