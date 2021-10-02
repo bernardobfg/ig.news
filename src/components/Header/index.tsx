94a83eb6e18ba2c86cb71b31dc6323327fb511a7
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-01 21:37:39 -0300

    configure CMS integration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a href="" className={styles.active}>Home</a>
-                    <a href="">Post</a>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active}>
+                        <a>Post</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton/>
             </div>
